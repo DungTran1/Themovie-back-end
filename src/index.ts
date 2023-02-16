8d035d5822891359a8bf2e11d5e5d083dd2cd4b2
DungTran1 <lssktt@gmail.com>
2023-02-16 11:15:53 +0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ var cookieParser = require("cookie-parser");
 var cors = require("cors");
 const app: Application = express();
 const corsOptions = {
-  origin: "https://themovie.onrender.com",
+  origin: process.env.PRODUCT_URL,
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
